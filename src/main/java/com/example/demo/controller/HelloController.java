8d202306cf30f3da1c08d72629b4287eeb079497
ramzibk <bk.ramzi@gmail.com>
2024-10-28 20:21:00 +0100

    add paths

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,11 +2,13 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 
 @Controller
+@Mapping("/api")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello(){
         return "Hello!";
     }
