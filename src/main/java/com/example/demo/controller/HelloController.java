f1a6d2241f017b50180ea860d1249dcd7b1b63d6
ramzibk <bk.ramzi@gmail.com>
2024-10-29 00:02:54 +0100

    fix controller

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,10 +1,9 @@
 package com.example.demo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller("/api")
+@RestController("/api")
 public class HelloController {
 
     @GetMapping("/hello")
